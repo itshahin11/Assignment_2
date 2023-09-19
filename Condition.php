c8f02bf6ebaf4825c439a387a49c975ebfd9fcf0
Shahin Alam <131897430+itshahin11@users.noreply.github.com>
2023-09-19 20:44:15 +0600

    Update Condition.php

--- Condition.php
+++ Condition.php
@@ -1,24 +1,18 @@
 <?php
+$a = 0;
+$b = 1;
+$i = 0;
 
+while ($i < 10) {
+  $c = $a + $b;
+  echo "$c\n";
 
-function calculateFibonacci($n) {
-    if ($n == 0) {
-        return 0;
-    } elseif ($n == 1) {
-        return 1;
-    } else {
-        return calculateFibonacci($n - 1) + calculateFibonacci($n - 2);
-    }
+  if ($c > 100) {
+    break;
+  }
+
+  $a = $b;
+  $b = $c;
+  $i++;
 }
-
-$count = 0;
-for ($i = 0; $count < 10; $i++) {
-    $fibonacciNumber = calculateFibonacci($i);
-
-    if ($fibonacciNumber > 100) {
-        break; // Break the loop if the Fibonacci number exceeds 100
-    }
-
-    echo $fibonacciNumber . " ";
-    $count++;
-}
+?>
